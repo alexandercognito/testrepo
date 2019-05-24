25f6a551bb6d3a8f83d57f348b071eab5d4404da
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-24 10:03:40 -0400

    Update setupHub.ps1

--- setupHub/setupHub.ps1
+++ setupHub/setupHub.ps1
@@ -5,6 +5,10 @@ $output = "C:\Selenium\SeleniumHubStartup.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 Expand-Archive -Path C:\Selenium\SeleniumHubStartup.zip -DestinationPath C:\Selenium
 
+$url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/SeleniumGridSetupService.exe"
+$output = "C:\Selenium\SeleniumGridSetupService.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
 Move-Item -Path 'C:\Selenium\Selenium Hub Startup\hubConfig.json' -Destination C:\Selenium\hubConfig.json
 Move-Item -Path 'C:\Selenium\Selenium Hub Startup\hub-start.cmd' -Destination C:\Selenium\hub-start.cmd
 Move-Item -Path 'C:\Selenium\Selenium Hub Startup\selenium-server-standalone-3.11.0.jar' -Destination C:\Selenium\selenium-server-standalone-3.11.0.jar
@@ -23,5 +27,8 @@ $javapath = ((Get-Item "C:/Program Files/Java/*/bin/java.exe" | Resolve-Path) -r
 
  New-NetFirewallRule -displayname SeleniumGridHub -direction inbound -action allow -protocol tcp -remotePort Any -localport 4444 | out-null
  New-NetFirewallRule -displayname SeleniumGridHub -direction outbound -action allow -protocol tcp -remotePort Any -localport 4444 | out-null
+
+#Start the service to setup a Selenium Grid Hub
+net start SeleniumGridSetupService /"$seleniumGridJarFile" /"$role" /"$hubRegisterUrl"  /"$configFile" /"$additionalParameters"
 
 Invoke-Item "C:\Selenium\hub-start.cmd"
