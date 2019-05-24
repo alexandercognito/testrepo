d99d3cda19f2694bae6d087b4ef8c5c70be06bee
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-24 09:45:24 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -1,5 +1,9 @@
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/setupNode.ps1"
 $output = "C:\setupNode.ps1"
+
+New-NetFirewallRule -displayname SeleniumGridNode -direction inbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
+New-NetFirewallRule -displayname SeleniumGridNode -direction outbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
+            
 If (-NOT([System.IO.File]::Exists($output)))
 {
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
