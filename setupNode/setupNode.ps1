beb52e76aa6b52e2af10f71f2eaf56bcefc63e02
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-27 13:16:22 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -1,6 +1,13 @@
 New-NetFirewallRule -displayname SeleniumGridNode -direction inbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
 New-NetFirewallRule -displayname SeleniumGridNode -direction outbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
 
+$usrname = 'DOMAINNAME\SAMACCOUNTNAME'
+$password = 'PASSWORD'
+$RegistryLocation = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Set-ItemProperty $RegistryLocation -Name 'AutoAdminLogon' -Value '1'
+Set-ItemProperty $RegistryLocation -Name 'DefaultUsername' -Value "$usrname"
+Set-ItemProperty $RegistryLocation -Name 'DefaultPassword' -Value "$password"
+
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/setupNode.ps1"
 $output = "C:\setupNode.ps1"
 
@@ -19,5 +26,3 @@ powershell "C:\setupNode.ps1"
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/SeleniumGridSetupService.exe"
 $output = "C:\Selenium\SeleniumGridSetupService.exe"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)            
-            
-
