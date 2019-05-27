98d270fc5be6bab8f7b39a1f5dab799825938001
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-27 11:33:00 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -1,13 +1,9 @@
+New-NetFirewallRule -displayname SeleniumGridNode -direction inbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
+New-NetFirewallRule -displayname SeleniumGridNode -direction outbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
+
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/setupNode.ps1"
 $output = "C:\setupNode.ps1"
 
-New-NetFirewallRule -displayname SeleniumGridNode -direction inbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
-New-NetFirewallRule -displayname SeleniumGridNode -direction outbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
-            
-$url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/SeleniumGridSetupService.exe"
-$output = "C:\Selenium\SeleniumGridSetupService.exe"
-(New-Object System.Net.WebClient).DownloadFile($url, $output)            
-            
 If (-NOT([System.IO.File]::Exists($output)))
 {
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
@@ -19,3 +15,9 @@ If (-NOT([System.IO.File]::Exists($output)))
     Start-Sleep -s 3
 }
 powershell "C:\setupNode.ps1"
+
+$url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/SeleniumGridSetupService.exe"
+$output = "C:\Selenium\SeleniumGridSetupService.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)            
+            
+
