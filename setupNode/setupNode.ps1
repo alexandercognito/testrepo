e35e29694b216490ff2d9b792d0fe04858fffc75
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-28 16:04:23 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -1,12 +1,6 @@
 New-NetFirewallRule -displayname SeleniumGridNode -direction inbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
 New-NetFirewallRule -displayname SeleniumGridNode -direction outbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
 
-#Schedule Selenium Start Node task to start at system start up
-$A = New-ScheduledTaskAction -Execute "C:\Selenium\selenium-start-node-3.5.2.cmd"
-$T = New-ScheduledTaskTrigger -AtLogon
-$D = New-ScheduledTask -Action $A -Trigger $T
-Register-ScheduledTask startNode -InputObject $D
-
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/setupNode.ps1"
 $output = "C:\setupNode.ps1"
 If (-NOT([System.IO.File]::Exists($output)))
