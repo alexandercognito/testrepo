a9e443d1854a79e8903fb03fdc5c52b57844636d
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-28 12:55:55 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -10,7 +10,6 @@ Register-ScheduledTask startNode -InputObject $D
 
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/setupNode.ps1"
 $output = "C:\setupNode.ps1"
-
 If (-NOT([System.IO.File]::Exists($output)))
 {
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
@@ -23,14 +22,15 @@ If (-NOT([System.IO.File]::Exists($output)))
 }
 powershell "C:\setupNode.ps1"
 
+
 $url = "https://live.sysinternals.com/Autologon.exe"
 $output = "C:\Autologon.exe"
-
 If (-NOT([System.IO.File]::Exists($output)))
 {
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
     Start-Sleep -s 3
 }
+
 
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/SeleniumGridSetupService.exe"
 $output = "C:\Selenium\SeleniumGridSetupService.exe"
