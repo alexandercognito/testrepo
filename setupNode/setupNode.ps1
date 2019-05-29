9b5089678ec4b20679766e1a48f437870960e73e
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-29 10:14:25 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -1,3 +1,7 @@
+param(
+    [int] $AutoLogonNumber = 1
+)
+
 New-NetFirewallRule -displayname SeleniumGridNode -direction inbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
 New-NetFirewallRule -displayname SeleniumGridNode -direction outbound -action allow -protocol tcp -remotePort Any -localport 5555 | out-null
 
@@ -14,16 +18,6 @@ If (-NOT([System.IO.File]::Exists($output)))
     Start-Sleep -s 3
 }
 powershell "C:\setupNode.ps1"
-
-
-$url = "https://live.sysinternals.com/Autologon.exe"
-$output = "C:\Autologon.exe"
-If (-NOT([System.IO.File]::Exists($output)))
-{
-    (New-Object System.Net.WebClient).DownloadFile($url, $output)
-    Start-Sleep -s 3
-}
-
 
 $url = "https://seleniumteststorage.blob.core.windows.net/seleniumstartup/SeleniumGridSetupService.exe"
 $output = "C:\Selenium\SeleniumGridSetupService.exe"
