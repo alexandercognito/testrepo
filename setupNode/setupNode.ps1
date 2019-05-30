d9d9951901b739faa46ff1df8eedb4ee59ee3a10
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-30 09:21:20 -0400

    Update setupNode.ps1

--- setupNode/setupNode.ps1
+++ setupNode/setupNode.ps1
@@ -28,5 +28,6 @@ powershell "C:\setupNode.ps1" -NodeNumber $NodeNumber -credential $Credential
 #Schedule Selenium Start Node task to start at system start up
 $A = New-ScheduledTaskAction -Execute "C:\Selenium\selenium-start-node-3.5.2.cmd"
 $T = New-ScheduledTaskTrigger -AtLogon
-$D = New-ScheduledTask -Action $A -Trigger $T
-Register-ScheduledTask startNode -InputObject $D         
+$P = New-ScheduledTaskPrincipal "TestUser"
+$D = New-ScheduledTask -Action $A -Principal $P -Trigger $T
+Register-ScheduledTask startNode -InputObject $D   
