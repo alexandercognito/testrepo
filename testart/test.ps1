f902c5cdd5beff1f4caffc74349f07d8dc75a514
alexandercognito <49795162+alexandercognito@users.noreply.github.com>
2019-05-30 09:09:49 -0400

    Update test.ps1

--- testart/test.ps1
+++ testart/test.ps1
@@ -5,5 +5,6 @@ param(
 #Schedule Selenium Start Node task to start at system start up
 $A = New-ScheduledTaskAction -Execute "C:\Selenium\selenium-start-node-3.5.2.cmd"
 $T = New-ScheduledTaskTrigger -AtLogon
-$D = New-ScheduledTask -Action $A -Trigger $T
+$P = New-ScheduledTaskPrincipal "System"
+$D = New-ScheduledTask -Action $A -Principal $P -Trigger $T
 Register-ScheduledTask startNode -InputObject $D   
